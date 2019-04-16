fbcc5e2523e7e2eeaf7e6143b08afee2c15febf5
crazy-lea <48503020+crazy-lea@users.noreply.github.com>
2019-04-16 12:34:25 +0800

    Update 线程池浅解.java

--- notes/线程池浅解.java
+++ notes/线程池浅解.java
@@ -20,10 +20,7 @@
                               RejectedExecutionHandler handler) {
     }
 
-    /**
-     *
-     * @return
-     */
+
     public static ExecutorService newCachedThreadPool() {
         /**
          * 创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程
